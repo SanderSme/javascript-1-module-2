8aadaac24de5d98e67857c7eee418213cdfa315d
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2022-01-31 13:59:32 +0100

    timeout

--- lesson-1/3-setTimeout/js/script.js
+++ lesson-1/3-setTimeout/js/script.js
@@ -1,2 +1,14 @@
 // The setTimeout method is used to execute a
 // function after a specified period of time.
+setTimeout(function (){
+console.log("hello i am Sander");
+}, 2000)//2sek
+
+//1. select the class .loader
+//use setTimeout
+//Change the inner HTML of the loading
+
+let loading = document.querySelector(".loader")
+setTimeout(function(){
+loading.innerHTML = "Finished downloading";
+}, 5000)
