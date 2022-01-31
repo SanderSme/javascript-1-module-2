ea5d32c5d29bc1d4c1e26022c56ca8b9bb2216fa
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2022-01-31 13:47:46 +0100

    lesson1module2

--- lesson-1/1-functions-as-arguments/js/script.js
+++ lesson-1/1-functions-as-arguments/js/script.js
@@ -2,3 +2,60 @@
 // to pass functions as arguments to other ***
 // functions and have the receiving function execute them.
 
+//Normal way of declearing a function
+function showMeMessage() {
+  console.log("normal function");
+}
+showMeMessage();
+
+//Calls itself
+(function doSomething() {
+  console.log("function that calls itself");
+})();
+
+//Hoisting(Call the function first, then make the function)
+hoisting();
+
+function hoisting() {
+    console.log("Hoisting example");
+}
+
+// //Anonymous function, then creating it. Will not work(error)
+// anonymousFunction();
+
+// const anonymousFunction = function() {
+//     console.log("Hoisting example two"); //Error
+// };
+
+//Callback functions(as arguments)
+function logTheArgument(someFunction) {
+    console.log(someFunction);
+}
+
+logTheArgument(function(){
+    console.log("I am a callback function");
+});
+
+//lesson-code
+function logSomething (someArgument) {
+    console.log(typeof someArgument);
+}
+
+logSomething("Sander");
+logSomething(1234567);
+
+const obj = {};
+logSomething(obj);
+
+const someArray = [];
+logSomething(someArray); //object
+
+function justSomeFunction(){
+    console.log("Hello i am a function");
+}
+logSomething(justSomeFunction); //function
+
+const justAHappyFunction = function(){
+    console.log("Hello i am a happy function");
+}
+logSomething(justAHappyFunction) //function
--- lesson-1/2-looping-dig-deeper/js/script.js
+++ lesson-1/2-looping-dig-deeper/js/script.js
@@ -12,7 +12,6 @@
 // Let's look at some of the most common
 // and useful iteration statements for arrays and objects.
 
-
 // I've set up an array called stuff that has some items
 // in it and a nested object called nested objects.
 
@@ -22,35 +21,89 @@
 const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser", "water bottle"];
 
 const nestedObjects = {
-    item01: {
-        name: "piggy",
-        type: "toy",
-        weight: 30,
-    },
-    item02: {
-        name: "headlamp",
-        type: "equipment",
-        weight: 120,
-    },
-    item03: {
-        name: "pen",
-        type: "tool",
-        weight: 30,
-    },
-    item04: {
-        name: "pencil",
-        type: "tool",
-        weight: 30,
-    },
-    item05: {
-        name: "eraser",
-        type: "tool",
-        weight: 40,
-    },
-    item06: {
-        name: "water bottle",
-        type: "equipment",
-        weight: 1300,
-    },
+  item01: {
+    name: "piggy",
+    type: "toy",
+    weight: 30,
+  },
+  item02: {
+    name: "headlamp",
+    type: "equipment",
+    weight: 120,
+  },
+  item03: {
+    name: "pen",
+    type: "tool",
+    weight: 30,
+  },
+  item04: {
+    name: "pencil",
+    type: "tool",
+    weight: 30,
+  },
+  item05: {
+    name: "eraser",
+    type: "tool",
+    weight: 40,
+  },
+  item06: {
+    name: "water bottle",
+    type: "equipment",
+    weight: 1300,
+  },
 };
 
+
+
+//1:
+//a: select the article
+//b: create a tag using JS ul
+//c: loop on the array
+//d: create another tag li
+//e: add items in the arrayn to the inner html of this tag
+//f: add all the new tags in the parent tag
+//add everything in the article tag
+
+const article = document.querySelector("article");
+
+let stuffList = document.createElement("ul");
+
+// for (let i = 0; i < stuff.length; i++) {
+//   let listItem = document.createElement("li");
+//   console.log(stuff[i]);
+
+//   listItem.innerHTML = stuff[i];
+//   console.log(listItem);
+
+//   stuffList.append(listItem);
+//   console.log(stuffList);
+// }
+
+//item of array
+//for of
+// for(const item of stuff) {
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = item;
+//     stuffList.append(listItem);
+// }
+
+//forEach
+stuff.forEach(function (item){
+    let listItem = document.createElement("li");
+    listItem.innerHTML = item;
+    stuffList.append(listItem);
+});
+
+stuff.forEach(() => {
+    //do stuff here
+});
+
+
+//Loop object
+for(let singleObject in nestedObjects){
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `Name: ${nestedObjects[singleObject].name}`;
+    stuffList.append(listItem);
+    }
+
+article.append(stuffList);
